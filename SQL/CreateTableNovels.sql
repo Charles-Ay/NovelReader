ce5d3ef8aeabd8a804b4f27b622df10461f2f241
Charles Ayeni <charles.ayeni@ihsmarkit.com>
2022-04-04 16:47:42 -0400

    change oneoff

--- SQL/CreateTableNovels.sql
+++ SQL/CreateTableNovels.sql
@@ -4,9 +4,9 @@ IF EXISTS(SELECT 1 FROM sys.columns
           WHERE Object_ID = Object_ID(N'dbo.Novels'))
 	DROP TABLE Novels
 CREATE TABLE Novels(
-	Name VARCHAR(255) PRIMARY KEY not null,
+	Name VARCHAR(255) not null,
 	Chapter int not null,
-	Link VARCHAR(255) not null,
+	Link VARCHAR(255) PRIMARY KEY not null,
 	Source VARCHAR(255),
 	FOREIGN KEY (Source) REFERENCES Sources(Name)
 )
